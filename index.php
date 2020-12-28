fa60cdcac86195e7eba2bc76649f4e75c3f821d8
Jani Ahmed <62384867+itsjaniahmed@users.noreply.github.com>
2020-12-28 13:06:12 -0800

    Starter template uploaded
    include bootstrap as a vendor and basically all directory for my project.

---
+++ index.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+  <meta name="generator" content="Hugo 0.79.0">
+  <title>YourScool</title>
+
+  <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/starter-template/">
+
+
+
+  <!-- Bootstrap core CSS -->
+  <link href="./include/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+
+
+
+  <!-- Custom styles for this template -->
+  <link href="./include/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <nav class="navbar navbar-expand-md navbar-light bg-warning fixed-top">
+    <div class="container-fluid">
+      <a class="navbar-brand " href="#">YourScool</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExampleDefault"
+        aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+          <li class="nav-item active">
+            <a class="nav-link" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-bs-toggle="dropdown"
+              aria-expanded="false">Dropdown</a>
+            <ul class="dropdown-menu" aria-labelledby="dropdown01">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+        </ul>
+       
+          <button class="btn btn-success m-2" type="submit">Login</button>
+          <button class="btn btn-primary" type="submit">Register</button>
+
+
+      </div>
+    </div>
+  </nav>
+
+  <main class="container">
+
+    <div class="starter-template text-center py-5 px-3">
+      <h1>Welcome, its YourScool</h1>
+      <p class="lead">Just enter on another level of education where we teach concept.</p>
+    </div>
+
+  </main><!-- /.container -->
+
+
+  <script src="./include/js/main.js"></script>
+  <script src="./include/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+
+</body>
+
+</html>
